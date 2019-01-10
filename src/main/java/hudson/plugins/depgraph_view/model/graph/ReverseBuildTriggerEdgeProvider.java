815160b7bda5fb3a844b11324f05c21328ab3f4b
lance513 <lance513@protonmail.com>
2019-01-10 11:18:26 +0100

    Add support for ReverseBuildTriggers of Pipeline projects

---
+++ src/main/java/hudson/plugins/depgraph_view/model/graph/ReverseBuildTriggerEdgeProvider.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2012 Stefan Wolf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.depgraph_view.model.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import hudson.model.Items;
+import hudson.triggers.Trigger;
+import jenkins.model.Jenkins;
+import jenkins.model.ParameterizedJobMixIn.ParameterizedJob;
+import jenkins.triggers.ReverseBuildTrigger;
+
+/**
+ * {@link EdgeProvider} yielding the dependencies of the Jenkins
+ * ReverseBuildTrigger property
+ */
+public class ReverseBuildTriggerEdgeProvider implements EdgeProvider {
+
+	@Inject
+	public ReverseBuildTriggerEdgeProvider(Jenkins jenkins) {
+	}
+
+	@Override
+	public Iterable<Edge> getEdgesIncidentWith(ParameterizedJob<?, ?> project) {
+
+		List<Edge> edges = new ArrayList<Edge>();
+
+		for (Trigger<?> trigger : project.getTriggers().values()) {
+			if (trigger instanceof ReverseBuildTrigger) {
+				for (ParameterizedJob<?, ?> upstream : Items.fromNameList(project.getParent(),
+						((ReverseBuildTrigger) trigger).getUpstreamProjects(), ParameterizedJob.class)) {
+					edges.add(new DependencyEdge(upstream, project));
+				}
+			}
+		}
+
+		for (ParameterizedJob<?, ?> downstream : project.getParent().allItems(ParameterizedJob.class)) {
+			for (Trigger<?> trigger : downstream.getTriggers().values()) {
+				if (trigger instanceof ReverseBuildTrigger && Items.fromNameList(project.getParent(),
+						((ReverseBuildTrigger) trigger).getUpstreamProjects(), ParameterizedJob.class)
+						.contains(project)) {
+					edges.add(new DependencyEdge(project, downstream));
+				}
+			}
+		}
+
+		return edges;
+	}
+
+}
